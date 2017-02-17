53e007eca24f959e1ab844fddc665fc2a651a1f3
narsa <narsa@DESKTOP-R6UVL6R>
2017-02-17 16:19:32 +0530

    Add project files.

---
+++ KECJobs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KECJobs.Startup))]
+namespace KECJobs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
